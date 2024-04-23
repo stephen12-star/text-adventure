b1985747edd6e82a67b1f20a95e22e11e8b5d932
poffi <143832574+poffi@users.noreply.github.com>
2024-04-23 10:44:52 -0700

    saved

--- game.js
+++ game.js
@@ -62,7 +62,7 @@ const textNodes = [
       },
       {
         text: 'Search the produce section.',
-        nextText: 5
+        nextText: 75
       },
       {
         text: 'henry Guards the door',
@@ -77,7 +77,7 @@ const textNodes = [
     options: [
       {
         text: 'Turn back',
-        nextText: 20
+        nextText: 35
       },
       {
         text: 'Scare rats off',
@@ -86,9 +86,34 @@ const textNodes = [
     ]
   },
 
+
+  {
+    id: 35,
+    text: 'you go back to your friend',
+    imgsrc: "storein.jpg",
+    options: [
+      {
+        text: 'search the produce section.',
+        nextText: 75
+      },
+      {
+        text: 'henry Guards the door',
+        nextText: 6
+      }
+    ]
+  },
+
+
+
+
+
+
+
+
   {
     id: 20,
     text: 'You show off your sword to everyone until you cant find henry',
+    imgsrc: "showoff.jpg",
     options: [
       {
         text: 'Look in the store for henry.',
@@ -98,7 +123,7 @@ const textNodes = [
   },
 
   {
-    id: 5,
+    id: 75,
     text: 'but you could not find any items, just a bunch of empty cans and broken glasses',
     imgsrc: "bottles.jpg",
     options: [
@@ -110,7 +135,7 @@ const textNodes = [
   },
   {
     id: 6,
-    text: ' In the process of guarding the door with herny an old woman appears in front of the door telling you about some mysterious entity but you decide to not give F***',
+    text: ' In the process of herny guarding the door an old woman appears in front of the door telling you about some mysterious entity but you decide to not give F***',
     imgsrc: "old.jpg",
     options: [
       {
@@ -122,7 +147,7 @@ const textNodes = [
   },
   {
     id: 9,
-    text: 'She begins mumbling a strange language before henry takes off running.',
+    text: 'She begins mumbling a strange language "tu ti shibu nito sinae" then henry takes off running.',
     imgsrc: "run.jpg",
     options: [
       {
@@ -168,7 +193,7 @@ const textNodes = [
       },
       {
         text: 'RUN AWAY',
-        nextText: 0
+        nextText: 90
       }
     ]
   },
@@ -219,7 +244,7 @@ const textNodes = [
       },
       {
         text: 'RUN',
-        nextText: 0
+        nextText: 90
       }
     ]
   },
@@ -232,6 +257,7 @@ const textNodes = [
   {
     id: 82,
     text: 'VICTORY!, YALL MEET EACH OTHER AND YALL GOT A CLUE SAYING "<I>THERE IS A BIGGER THREAT AHEAD<I>"',
+    imgsrc: "meeting.jpg",
     options: [
       {
         text: 'CONTINUE LOOKING FOR HENRY',
@@ -239,7 +265,26 @@ const textNodes = [
       },
       {
         text: 'GIVE UP',
-        nextText: 0
+        nextText: 90
+      }
+    ]
+  },
+
+
+
+
+  {
+    id: 102,
+    text: 'VICTORY! but you find out that a hint says...."<I>THERE IS A BIGGER THREAT AHEAD<I>"',
+    imgsrc: "meeting.jpg",
+    options: [
+      {
+        text: 'CONTINUE LOOKING FOR HENRY',
+        nextText: 83
+      },
+      {
+        text: 'GIVE UP',
+        nextText: 90
       }
     ]
   },
@@ -251,7 +296,7 @@ const textNodes = [
   {
     id: 83,
     text: 'THE PROCESS OF LOOKING OF HENRY A FOGGY STEAM APPEARS IN FRONT OF YOU AND YOU FIND OUT IT WAS THE FINAL BOSS AHEAD',
-
+    imgsrc: "skull.jpg",
     options: [
       {
         text: 'USE THE MIRROR',
@@ -271,8 +316,8 @@ const textNodes = [
 
   {
     id: 85,
-    text: 'THE DRAGGER DID NOT WORK,, YOU DIED',
-   
+    text: 'THE DRAGGER DID NOT WORK, YOU DIED',
+    imgsrc: "death.jpg",
     options: [
       {
         text: 'RESTART',
@@ -287,7 +332,7 @@ const textNodes = [
   {
     id: 86,
     text: 'VICTORY',
-   
+
     options: [
       {
         text: 'Congratulations. Play Again.',
@@ -304,20 +349,32 @@ const textNodes = [
     options: [
       {
         text: 'KILL A FRIEND',
-        nextText: 8
+        nextText: 1000
       },
       {
         text: 'GIVE UP',
-        nextText: 0
+        nextText: 90
+      }
+    ]
+  },
+
+  {
+    id: 100,
+    text: 'with the blood of the friend you used it to activate the mirror, victory you sucked in all his power.',
+
+    options: [
+      {
+        text: 'Congratulations. Play Again.',
+        nextText: -1
       }
     ]
   },
 
 
-
   {
     id: 80,
     text: 'YOU DIED BECAUSE U NEED TO COVER YOUR EAR FIRST',
+    imgsrc: "death.jpg",
     options: [
       {
         text: 'RESTART',
@@ -333,18 +390,47 @@ const textNodes = [
 
   {
     id: 16,
-    text: 'You go inside and tell everyone Henry ran off',
+    text: 'You go inside and tell everyone Henry ran off, so you guys decided to team together and find him or not.',
+    imgsrc: "storein.jpg",
     options: [
       {
-        text: 'Say he ran off',
+        text: 'stay together',
         nextText: 11
       },
       {
-        text: 'Say a weird woman scared him off',
-        nextText: 12
+        text: 'split off',
+        nextText: 103
       }
     ]
   },
+
+
+
+
+
+  {
+    id: 103,
+    text: 'so there is three different path with three different monsters, pick which path to go...',
+    imgsrc: "path.jpg",
+    options: [
+      {
+        text: ' Take path 1',
+        nextText: 17
+      },
+      {
+        text: ' Take path 2',
+        nextText: 18
+      },
+      {
+        text: ' Take path 3',
+        nextText: 19
+      }
+    ]
+  },
+
+
+
+
 
 
 
@@ -355,22 +441,35 @@ const textNodes = [
       {
         text: 'Take sword',
         setState: { Sword: true },
-        nextText: 13
+        nextText: 20
 
       },
       {
         text: 'Leave sword',
-        nextText: 14
+        nextText: 35
       }
     ]
   },
+
+
+
+  
+
+
+
+
+
+
+
+
   {
-    id: 8,
+    id: 100,
     text: 'Your attempts to run are in vain and the monster easily catches.',
+    imgsrc: "death.jpg",
     options: [
       {
         text: 'Restart',
-        nextText: -1
+        nextText: 90
       }
     ]
   },
@@ -380,7 +479,7 @@ const textNodes = [
     options: [
       {
         text: 'Restart',
-        nextText: -1
+        nextText: 90
       }
     ]
   },
@@ -390,19 +489,21 @@ const textNodes = [
     options: [
       {
         text: 'Restart',
-        nextText: -1
+        nextText: 90
       }
     ]
   },
   {
     id: 11,
-    text: 'You threw your jar of goo at the monster and it exploded. After the dust settled you saw the monster was destroyed. Seeing your victory you decide to claim this castle as your and live out the rest of your days there.',
+    text: 'so yall stayed together and yall left the store to look for him towards the woods',
+    imgsrc: "woodsss.webp",
     options: [
       {
-        text: 'Congratulations. Play Again.',
-        nextText: -1
+        text: 'next',
+        nextText: 200
       }
     ]
+    //so this part will link to the side they decide to stay together so connect this to the part they fought the monster together.
   },
   {
     id: 50,
@@ -425,7 +526,7 @@ const textNodes = [
     ]
   },
   {
-    id: 52, 
+    id: 52,
     text: 'He says his stomach isnt feeling well, we try to look for a hotel to rest at until henry says hes gonna puke. When we turned back to check on him there was no trace of him',
     options: [
       {
@@ -436,12 +537,83 @@ const textNodes = [
   },
 
   {
-    id: 53, 
+    id: 53,
     text: 'Everyone begins to panic, we retrace our steps but there is no sign of Henry. Sam says to split up but Chris disagrees.',
     options: [
       {
         text: ' Agree to split',
-        nextText: 54
+        nextText: 103
+      },
+      {
+        text: 'Stay together',
+        nextText: 11
+      }
+
+    ]
+  },
+
+
+  {
+    id: 200,
+    text: 'You guys followed a path from a map and got led to many monsters',
+    options: [
+      {
+        text:'Prepare for a fight.',
+        nextText: 17
+      }
+    ]
+  },
+
+
+
+
+
+  {
+    id: 17,
+    text: 'You encounter an Arachne, in order to defeat it you have to think of the best way.',
+    imgsrc: "spider.jpg",
+    options: [
+      {
+        text: 'USE THE SWORD YOU FOUND',
+        nextText: 18
+      },
+      {
+        text: 'RUN AWAY',
+        nextText: 90
+      }
+    ]
+  },
+
+
+  {
+    id: 18,
+    text: 'You encounter a vampire, in order to defeat it you have to think of the best way.',
+    imgsrc: "vammm.jpg",
+    options: [
+      {
+        text: 'USE THE WOODEN STICK NEXT TO YOU',
+        nextText: 19
+      },
+      {
+        text: 'DECIDED TO RUN',
+        nextText: 12
+      }
+    ]
+  },
+
+
+  {
+    id: 19,
+    text: 'You encounter a shapeshifter, in order to defeat it you have to think of the best way.',
+    imgsrc: "goddess.png",
+    options: [
+      {
+        text: 'COVER YOUR EAR',
+        nextText: 81
+      },
+      {
+        text: 'USE THE DRAGGER NEXT TO YOU',
+        nextText: 80
       }
     ]
   },
@@ -454,10 +626,10 @@ const textNodes = [
 
 
 
-
   {
-    id: 0,
+    id: 90,
     text: 'YOU DIED',
+    imgsrc: "death.jpg",
     options: [
       {
         text: 'RESTART',
